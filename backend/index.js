e15bb0c331cd2e253604d6385086ef2657e2d76e
aortegas <antonioortega@uma.es>
2024-12-12 22:20:01 +0100

    front

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ const app = require('./src/app'); // Import the Express app
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://admin:admin@localhost:27017/exam?authSource=admin';
 
-console.log('Connecting to database:', MONGODB_URI);
 mongoose.connect(MONGODB_URI)
     .then(() => {
+        console.log('Connected to DB');
         app.listen(PORT, () => {
             console.log(`Server is running on http://localhost:${PORT}`);
         });
---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="bg-green-500">
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        TEST
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+
+  <div>
+    TEST
+  </div>
+</template>
