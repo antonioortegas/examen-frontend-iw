511dc1fe7ec5be845a16150a63149bff3d71b48b
aortegas <antonioortega@uma.es>
2024-12-13 01:23:59 +0100

    backend for photos

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,13 @@ const express = require('express');
 
 const taskRouter = require('./routers/taskRouter');
 const colaboradoresRouter = require('./routers/collaboratorRouter');
+const cloudinaryRouter = require('./routers/cloudinaryRouter');
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/tasks', taskRouter);
 app.use('/api/collaborators', colaboradoresRouter);
+app.use('/api/cloudinary', cloudinaryRouter);
 
 module.exports = app;
