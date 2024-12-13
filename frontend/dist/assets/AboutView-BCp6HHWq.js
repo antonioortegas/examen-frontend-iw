224132ba438ae792b8ef2e0d1e3d658639604f00
aortegas <antonioortega@uma.es>
2024-12-13 16:48:04 +0100

    commit

--- frontend/dist/assets/AboutView-BCp6HHWq.js
+++
@@ -1,1 +0,0 @@
-import{c,a as s,o as r}from"./index-DQ6XZyvv.js";const l=(e,t)=>{const o=e.__vccOpts||e;for(const[n,a]of t)o[n]=a;return o},_={},i={class:"about"};function u(e,t){return r(),c("div",i,t[0]||(t[0]=[s("h1",null,"This is an about page",-1),s("div",null," FRASE ",-1)]))}const f=l(_,[["render",u]]);export{f as default};
---
+++ frontend/dist/assets/AboutView-DTuSRP2u.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{c as s,a as o,o as a}from"./index-B99EXYMX.js";const n={},r={class:"about"};function c(i,e){return a(),s("div",r,e[0]||(e[0]=[o("h1",null,"This is an about page",-1),o("div",null," FRASE ",-1)]))}const u=t(n,[["render",c]]);export{u as default};
